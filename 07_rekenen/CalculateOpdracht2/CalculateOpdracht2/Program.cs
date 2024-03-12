d7f8a8add4625a2f2ca7bffdb3773bb5f199f41b
Evert <37932@ma-web.nl>
2024-03-12 09:39:55 +0100

    rekenen

--- 07_rekenen/CalculateOpdracht2/CalculateOpdracht2/Program.cs
+++ 07_rekenen/CalculateOpdracht2/CalculateOpdracht2/Program.cs
@@ -14,25 +14,28 @@ namespace CalculateOpdracht2
 
 
             //koop 2 controllers
+            controllersInMandje += 2;
 
 
             //hij is in voorraad snel kopen!
             int ps5 = 0;
 
             //koop 1 ps5
+            ps5 += 1;
 
 
             //koop de games:
 
-            int eldenRing = 0;
+            int eldenRing = 1;
 
-            int shantae = 0;
+            int shantae = 1;
 
-            int spongebob = 0;
+            int spongebob = 1;
 
 
             //hmmm weten we het zeker?
             //haal 1 game weg met -- of -= 1
+            spongebob -= 1;
 
             //nu nog de prijs
 
@@ -41,13 +44,13 @@ namespace CalculateOpdracht2
             double controllerPrijs = 69.99;
             double gamePrijs = 59.99;
 
+            double totaalps5Prijs = ps5 * ps5Prijs;
+            double totaalControllerPrijs = controllersInMandje*controllerPrijs;// controllersInMandje keer controllerPrijs
+            double totaalGamePrijs = (shantae* gamePrijs)+ (eldenRing * gamePrijs);// game1 keer gamePrijs  + game2 keer gamePrijs (let op je haakjes!)
 
-            double totaalControllerPrijs = 0;// controllersInMandje keer controllerPrijs
-            double totaalGamePrijs = 0;// game1 keer gamePrijs  + game2 keer gamePrijs (let op je haakjes!)
 
 
-
-            double totaalPrijs = 0;// tel hier alles bij elkaar op
+            double totaalPrijs = totaalps5Prijs+ totaalControllerPrijs+ totaalGamePrijs;// tel hier alles bij elkaar op
 
             //er moet nu 759.95 staan
         }
